59c1f1d4c4f63e37e7e38f36606e195d7a644a5c
Leamdra <100883313+Leamdra@users.noreply.github.com>
2022-03-30 21:49:40 -0300

    Add files via upload

---
+++ aula 3003/script.js
@@ -0,0 +1,33 @@
+//function criarParagrafo() {
+  //  let para = document.createElement('p');
+    //para.textContent = 'E 1, 2, 3, 4, 5, 6, 7, 8, 9 e 10';
+    //document.body.appendChild(para);
+    //}
+
+//const botoes = document.querySelectorAll('button');
+//for(var i = 0; i < botoes.length ; i++){
+//botoes[i].addEventListener('click', criarParagrafo);
+
+//}
+
+ function Ate10(){
+     let para = document.createElement('p');
+     para.textContent = '1,2,3,4,5,6,7,8,9,10';
+     document.body.appendChild(para);
+ }
+
+ function debtec(){
+     let para = document.createElement('p');
+     para.textContent = 'Também conhecido como dívida técnica, o débito técnico é um conceito no desenvolvimento de software utilizado para representar o custo implícito de uma implementação ou solução pensada somente no agora, elaborada para suprir demandas atuais, em vez fazer uso de uma abordagem de melhor qualidade. O motivo por trás de tudo isso? Diminuir o tempo de lançamento de determinado produto de software.';
+     document.body.appendChild(para);
+
+ }
+
+ function sonarlink(){
+    window.open("https://blog.myscrumhalf.com/sonar-apoiando-a-qualidade-do-desenvolvimento-de-software/#:~:text=O%20Sonar%20%C3%A9%20uma%20ferramenta,da%20qualidade%20do%20seu%20c%C3%B3digo." , "_blank");
+ }
+
+ function jankinslink(){
+   
+    window.open("https://www.gocache.com.br/dicas/para-que-serve-jenkins-e-o-que-ele-pode-fazer/", "_blank");
+ }
